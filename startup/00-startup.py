7dd64600b9da90aeb4048fa8a76aea6a7eda5fb1
Charles Titus <ctitus@bnl.gov>
2022-02-22 15:11:06 -0500

    RE configuration removed and put in ucal_common

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -1,10 +1,5 @@
 import nslsii
 import ucal_common as ucal
 from ucal_common.startup import *
-from ucal_common.run_engine import setup_run_engine
-from bluesky import RunEngine
 
-RE = RunEngine(call_returns_result=True)
-RE = setup_run_engine(RE)
 nslsii.configure_base(get_ipython().user_ns, "ucal")
-
