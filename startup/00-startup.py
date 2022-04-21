4cac6aec9485edbbf761443a0495b2dac5c49e04
Charles Titus <ctitus@bnl.gov>
2022-04-21 11:59:17 -0400

    pre-upgrade changes

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -1,5 +1,17 @@
 import nslsii
 import ucal_common as ucal
 from ucal_common.startup import *
-
+from bluesky.callbacks import LiveTable
 nslsii.configure_base(get_ipython().user_ns, "ucal")
+
+loadfile = beamline_config.get('loadfile', None)
+if loadfile is not None:
+    if beamline_config.get('bar', '') == 'Standard 4-sided bar':
+        load_standard_four_sided_bar(loadfile)
+        sampleholder.print_samples()
+# sd SupplementalData preprocessor is automatically loaded and
+# subscribed to RunEngine by nslsii.configure_base
+sd.baseline.append(eslit)
+
+# Fix LiveTable display
+LiveTable._FMT_MAP['number'] = 'g'
