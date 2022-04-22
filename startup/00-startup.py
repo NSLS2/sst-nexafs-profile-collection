7f4f4f8023ff4fa8e6cdacb3c35588d36dca0615
maffettone <phillip.maffettone@gmail.com>
2022-04-22 12:08:34 -0400

    Extend sys paths for collection packages

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -1,12 +1,26 @@
+import sys
+from pathlib import Path
+
+paths = [
+    path
+    for path in Path(
+        "/nsls2/data/sst1/ucal/shared/config/bluesky/collection_packages"
+    ).glob("*")
+    if path.is_dir()
+]
+for path in paths:
+    sys.path.append(path)
+
 import nslsii
 import ucal_common as ucal
 from ucal_common.startup import *
 from bluesky.callbacks import LiveTable
+
 nslsii.configure_base(get_ipython().user_ns, "ucal", publish_documents_with_kafka=True)
 
-loadfile = beamline_config.get('loadfile', None)
+loadfile = beamline_config.get("loadfile", None)
 if loadfile is not None:
-    if beamline_config.get('bar', '') == 'Standard 4-sided bar':
+    if beamline_config.get("bar", "") == "Standard 4-sided bar":
         load_standard_four_sided_bar(loadfile)
         sampleholder.print_samples()
 # sd SupplementalData preprocessor is automatically loaded and
@@ -14,4 +28,4 @@ if loadfile is not None:
 sd.baseline.append(eslit)
 
 # Fix LiveTable display
-LiveTable._FMT_MAP['number'] = 'g'
+LiveTable._FMT_MAP["number"] = "g"
