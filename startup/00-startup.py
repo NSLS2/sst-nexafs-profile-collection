20276bc05d042e25dbe0f8d475d1015819a658d2
Charles Titus <ctitus@bnl.gov>
2024-02-27 17:26:36 -0500

    added working device config to profile

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -14,18 +14,23 @@ for path in paths:
 import nslsii
 import ucal
 from ucal.startup import *
+from ucal.plans.alignment import load_saved_manipulator_calibration
 from bluesky.callbacks import LiveTable
 
 nslsii.configure_base(get_ipython().user_ns, "ucal", publish_documents_with_kafka=True)
 
 loadfile = beamline_config.get("loadfile", None)
-if loadfile is not None:
+if loadfile is not None and loadfile != "":
     if beamline_config.get("bar", "") == "Standard 4-sided bar":
         load_standard_four_sided_bar(loadfile)
         sampleholder.print_samples()
+
+print("Loading last saved manipulator calibration")
+RE(load_saved_manipulator_calibration())
+
 # sd SupplementalData preprocessor is automatically loaded and
 # subscribed to RunEngine by nslsii.configure_base
-sd.baseline.append(eslit)
+#sd.baseline.append(eslit)
 
 # Fix LiveTable display
-LiveTable._FMT_MAP["number"] = "g"
+#LiveTable._FMT_MAP["number"] = "g"
