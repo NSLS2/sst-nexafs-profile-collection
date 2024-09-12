3dea6a07f82f232b6007a4ae14b370195663ed3b
Charles Titus <ctitus@bnl.gov>
2024-09-11 20:16:49 -0400

    TES now uses filename pattern again, 00-startup modified for new tiled API keys

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -13,7 +13,8 @@ for path in paths:
     sys.path.append(str(path))
 """
 
-import nslsii
+from nslsii import configure_kafka_publisher, configure_bluesky_logging, configure_ipython_logging
+from nslsii.common.ipynb.logutils import log_exception
 #import ucal
 #from ucal.startup import *
 #from ucal.plans.alignment import load_saved_manipulator_calibration
@@ -21,10 +22,41 @@ import nslsii
 from bluesky_queueserver import is_re_worker_active
 
 from nbs_bl.configuration import load_and_configure_everything
+from tiled.client import from_profile
+import os
+
 
 load_and_configure_everything()
 
-nslsii.configure_base(get_ipython().user_ns, "ucal", bec=False, publish_documents_with_kafka=True)
+# nslsii.configure_base(get_ipython().user_ns, "ucal", bec=False, publish_documents_with_kafka=True)
+ipython = get_ipython()
+
+configure_bluesky_logging(ipython=ipython)
+    
+# IPython logging will be enabled with logstart(...)
+configure_ipython_logging(exception_logger=log_exception, ipython=ipython)
+    
+ipython.run_line_magic("xmode", "minimal")
+
+tiled_writing_client = from_profile("nsls2", api_key=os.environ["TILED_BLUESKY_WRITING_API_KEY_NEXAFS"])['ucal']['raw']
+
+def post_document(name, doc):
+    ATTEMPTS = 20
+    error = None
+    for attempt in range(ATTEMPTS):
+        try:
+            tiled_writing_client.post_document(name, doc)
+        except Exception as e:
+            print(f"Document saving failure: {e!r}")
+            error = e
+        else:
+            break
+        time.sleep(2)
+    else:
+        raise error
+
+RE.subscribe(post_document)
+configure_kafka_publisher(RE, beamline_name="ucal")
 
 if is_re_worker_active():
     RE.waiting_hook = None
